119dcead2b3431e9c5907436dc20cacfe7c19395
Hugo Santana <hugosc@id.uff.br>
2017-06-25 17:57:04 -0300

    V(0) = 2PI + 0.1 (*1.pdf = x por vx, *2.pdf = r por v)

--- graficos.py
+++ graficos.py
@@ -19,6 +19,7 @@ class Planeta:
 		self.r= np.sqrt(x**2+y**2)
 		self.vx= vx
 		self.vy= vy
+		self.v= np.sqrt(vx**2+vy**2)
 		self.e= 0.5*massa*(vx**2+vy**2) - GM*m/self.r
 		
 	def a(self, pos):
@@ -34,21 +35,29 @@ class Planeta:
 		self.y= self.y + self.vy*dt + 0.5*aty*dt**2
 		self.vy= self.vy + aty*dt
 		self.e= 0.5*m*(self.vx**2+self.vy**2) - GM*m/self.r
+		self.r= np.sqrt(self.x**2+self.y**2)
+		self.v= np.sqrt(self.vx**2+self.vy**2)
 		
-p1=Planeta(1,1,0,0,2*np.pi)
+p1=Planeta(1,1,0,0,2*np.pi)#
 	
 	
 tmax=4
 t=np.arange(0, tmax, dt)
 x=np.zeros(t.size)
 y=np.zeros(t.size)
+r=np.zeros(t.size)
 vx=np.zeros(t.size)
 vy=np.zeros(t.size)
+v=np.zeros(t.size)
 
 for i in range (t.size):
 	p1.move(t[i])
 	x[i]=p1.x
 	y[i]=p1.y
+	r[i]=p1.r
+	vx[i]=p1.vx
+	vy[i]=p1.vy
+	v[i]=p1.v
 	
 plt.figure(figsize=(8,5),dpi=100)
 plt.axes().set_aspect('equal','datalim')
@@ -63,10 +72,10 @@ ax.autoscale()
 
 plt.rc('text',usetex=True)
 plt.rc('font',**{'sans-serif':'Arial','family':'sans-serif'})
-plt.title(r'\textnormal{Movimento Planetario}',fontsize=12)
+plt.title(r'\textnormal{Espaco de fases (x por vx)}',fontsize=12)#
 
-plt.plot (y,x, 'r-', linewidth=1)
+plt.plot (x,vx, 'r-', linewidth=1)#
 plt.legend(loc="upper right")
 plt.grid()
-plt.savefig("mov1.pdf",dpi=96)
+plt.savefig("espacodefases1.pdf",dpi=96)#
 plt.show()	
