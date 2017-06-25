3b13cec9f3ea17cfeb524726eb99e0e2359b1646
Hugo Santana <hugosc@id.uff.br>
2017-06-25 16:33:10 -0300

    Duas estrelas e um planeta (posicao0(0.1,1))

--- solterra.py
+++ solterra.py
@@ -17,14 +17,14 @@ class Planeta:
 		self.m= massa
 		self.x= x
 		self.y= y
-		self.r= np.sqrt(x**2+y**2)
+		self.r1= np.sqrt((x-1)**2+y**2)
+		self.r2= np.sqrt((x+1)**2+y**2)
 		self.vx= vx
 		self.vy= vy
-		self.e= 0.5*massa*(vx**2+vy**2) - GM*m/self.r
+		self.e= 0.5*massa*(vx**2+vy**2) - (GM*m/self.r1 + GM*m/self.r2)
 		
 	def a(self, pos):
-		r= np.sqrt(self.x**2+self.y**2)
-		at= -(GM/r**3)*pos
+		at= -(GM/self.r1**3 + GM/self.r2**3)*pos
 		return at
 		
 	def move(self):
@@ -34,9 +34,9 @@ class Planeta:
 		aty= self.a(self.y)
 		self.y= self.y + self.vy*dt + 0.5*aty*dt**2
 		self.vy= self.vy + aty*dt
-		self.e= 0.5*m*(self.vx**2+self.vy**2) - GM*m/self.r
+		self.e= 0.5*m*(self.vx**2+self.vy**2) - (GM*m/self.r1 + GM*m/self.r2)
 
-terra=Planeta(1,1,0,0,2*np.pi)
+terra=Planeta(1,0.1,1,0,0)
 
 def wrap_angle(angle):
 	return angle%360
@@ -48,17 +48,19 @@ myfont = pygame.font.Font(None,60) #fonte, tamanho
 space = pygame.image.load("space.png").convert() # carrega imagem
 planet = pygame.image.load("planeta.png").convert_alpha() 
 sun = pygame.image.load("sun.png").convert_alpha() #alpha = transparencia
+sun = pygame.transform.scale(sun,(50,50))
 
 sunw,sunh = sun.get_size() #transforma em uma tupla
-pygame.display.set_caption("O Sol e a Terra")
+pygame.display.set_caption("Duas estrelas e um planeta")
 
 while True:
 	for event in pygame.event.get():
 		if event.type in (QUIT,KEYDOWN):
 			sys.exit()
-	planet = pygame.transform.scale(planet,(50,50))
+	planet = pygame.transform.scale(planet,(40,40))
 	screen.blit(space, (0,0))
-	screen.blit(sun, (300-sunw/2, 300-sunh/2))
+	screen.blit(sun, (25, 300-25))
+	screen.blit(sun, (600-75, 300-25))
 	xant,yant = terra.x, terra.y
 	terra.move()
 	dx = terra.x - xant
@@ -66,5 +68,5 @@ while True:
 	angulo = math.atan2(dy,dx)
 	angulo = wrap_angle(-math.degrees(angulo)+90)
 	dayplanet = pygame.transform.rotate(planet,angulo)
-	screen.blit(dayplanet, (terra.x*250+275,terra.y*250+275))
+	screen.blit(dayplanet, (terra.x*(600-20)/2 + (600-20)/2, terra.y*(600-20)/2 + (600-20)/2))
 	pygame.display.update()
